1f92fc191cafa9b1849622f08d67b8b9a192b48e
Will Speak <lithiumflame@gmail.com>
2013-04-20 16:57:18 +0100

    Update Library Tests
    The test now uses the `library_local` linkage as well as the `api_export` linkage

--- test/test_lib_bar.c
+++ test/test_lib_bar.c
@@ -2,7 +2,13 @@
 
 #include "test_lib_bar.h"
 
+library_local int get_return_code(void)
+{
+	// This is the correct function function
+	return 0;
+}
+
 api_export int test_lib_bar_global(void)
 {
-	return 0;
+	return get_return_code();
 }
--- test/test_lib_foo.c
+++ test/test_lib_foo.c
@@ -3,6 +3,12 @@
 #include "test_lib_foo.h"
 #include "test_lib_bar.h"
 
+library_local int get_return_code(void)
+{
+	// This is the wrong function
+	return 1;
+}
+
 api_export int test_lib_foo_global(void)
 {
 	return test_lib_bar_global();
